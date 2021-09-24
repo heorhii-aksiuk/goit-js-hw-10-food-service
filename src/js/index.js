49cac29542489bd83811a63fa5e915889e232e03
Heorhii Aksiuk <aksiuk.heorhii@gmail.com>
2021-09-24 20:37:27 +0300

    Создание динамической разметки

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,11 @@
+import productsMarkup from '../templates/produrcts.handlebars';
+import data from '../menu.json';
 import refs from './refs';
 import { setUserTheme } from './change-theme';
 import { changeTheme } from './change-theme';
-const { checkboxEl, listEl, bodyEl } = refs;
+const { checkboxEl, listEl } = refs;
 
 setUserTheme();
 checkboxEl.addEventListener('change', changeTheme);
+
+listEl.insertAdjacentHTML('afterbegin', productsMarkup(data));
